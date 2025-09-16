f6b91554cd7449a21e4a714429e0bc0c10904adc
TheSixDecadence <DelosSantosJ.10.3A@gmail.com>
2025-09-16 13:24:26 -0600

    Correcion de tipeado para buildeo correcto

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -4,12 +4,14 @@ import { prisma } from '@/lib/prisma'
 // GET /api/products/[id] - Get a specific product
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const { id } = await params;
+
     const product = await prisma.product.findUnique({
       where: {
-        id: params.id
+        id: id
       }
     })
 
